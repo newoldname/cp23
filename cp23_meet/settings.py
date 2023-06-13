04c1066327cc65ec231e44052f266aba5a8713c0
newoldname <thjbjs@outlook.com>
2023-06-14 03:08:26 +0900

    [feat] CF 관련 작업 중-업로드까지 해봄

--- cp23_meet/settings.py
+++ cp23_meet/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-1al056g*(cw02+ui9*e+ptj_a_d=kx3l7ck9(a6i6e#f$j^c!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
